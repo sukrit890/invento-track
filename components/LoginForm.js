57acccce2e74beee85493607061bd3b01ae23edf
Sukrit Priyank <sukritp2004@gmail.com>
2024-07-19 13:09:13 +0530

    changed background

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -36,10 +36,9 @@ const router= useRouter('');
     <>
     <Headerf />
 
-    
     <div className="min-h-screen flex items-center justify-center relative">
-      {/* Background with radial gradient */}
-      <div className="absolute inset-0 z-0 h-full w-full items-center px-5 py-24" style={{ background: 'radial-gradient(125% 125% at 50% 10%, #000 40%, #63e 100%)' }}></div>
+  {/* Background with radial gradient */}
+  <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
 
 
 
